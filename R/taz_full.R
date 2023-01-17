099c75d1201095f3d6f9943a3ec8b1fe1e35218c
kssrr <lennart.kasserra@gmail.com>
2023-01-17 23:08:37 +0100

    fix taz dates

--- R/taz_full.R
+++ R/taz_full.R
@@ -7,6 +7,7 @@ library(xml2)
 library(purrr)
 library(stringr)
 library(parallel)
+library(lubridate)
 library(pbmcapply)
 
 url <- "https://taz.de/sitemap-index.xml"
@@ -68,6 +69,18 @@ get_date <- function(src) {
   return(out)
 }
 
+# Sometimes, the date is not included in the meta, but is more freely
+# written in a div (e.g. "Monday, 28.11.2020, 18:59"). This function 
+# tries to still extract it. 
+get_date2 <- function(src) {
+  out <- src |> 
+    html_elements("div") |> 
+    html_elements(xpath = '//*[@class="dateLocWrapper"]') |> 
+    html_text2()
+  if (rlang::is_empty(out)) return(NA_character_)
+  return(out)
+}
+
 patterns <- c(
   '//p[@class="article first odd"]',
   '//p[@class="article first odd initial"]',
@@ -91,20 +104,23 @@ cores <- detectCores()
 taz_full <- pbmclapply(
   article_urls,
   function(url) {
+    Sys.sleep(runif(1))
     tryCatch(
       expr = {
-        article <- rvest::read_html(url)
+        article <- rvest::read_html(httr::GET(url))
         
         data.frame(
           url         = url,
-          date        = get_meta(article, "date"),
+          date        = get_date(article),
+          date_alt    = NA_character_,
           title       = get_title(article),
           author      = get_meta(article, "author"),
           description = get_meta(article, "description"),
           keywords    = get_meta(article, "keywords"),
           body        = get_body(article),
           error       = NA_character_
-        )
+        ) |> 
+          mutate(date_alt = ifelse(is.na(date), get_date2(article), date_alt))
       },
       error = function(e) {
         e <- as.character(e[1])
@@ -114,6 +130,7 @@ taz_full <- pbmclapply(
         data.frame(
           url         = url,
           date        = NA_character_,
+          date_alt    = NA_character_,
           title       = NA_character_,
           author      = NA_character_,
           description = NA_character_,
@@ -129,4 +146,35 @@ taz_full <- pbmclapply(
 )
 
 taz_full <- tibble(do.call(rbind, taz_full))
+
+# Unifying date column from the freely extracted dates. Ideally,
+# this would be dealt with in get_date2, a rewrite would be appreciated.
+taz_full <- taz_full |> 
+  mutate(
+    date_alt = map_chr(date_alt, ~ {
+      .x |>   
+        str_remove_all("[a-zA-Z]|,") |> 
+        trimws()
+    }),
+    time = trimws(str_extract(date_alt, "\\s(.*?)$")),
+    date_alt = map_chr(date_alt, ~ {
+      .x |> 
+        str_remove("\\s(.*?)$") |> 
+        str_replace_all("\\.", "-") |> 
+        lubridate::dmy() |> 
+        as.character()
+    }),
+    date_alt = ifelse(
+      !is.na(date_alt),
+      paste0(date_alt, "T", time, ":00+01:00"),
+      date_alt
+    ),
+    date = ifelse(is.na(date), date_alt, date)
+  ) |> 
+  select(-c(date_alt, time)) |> 
+  distinct()
+
+# Also cleaning titles:
+taz_full <- taz_full |> mutate(title = str_remove(title, " - taz.de$"))
+
 write.csv(taz_full, "taz_full.csv")
