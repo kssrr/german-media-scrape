edcb91c086c0348061b6ffdc6a66159c522d4407
kssrr <lennart.kasserra@gmail.com>
2023-01-22 22:10:28 +0100

    rewrite zeit

--- R/zeit_full.R
+++ R/zeit_full.R
@@ -5,7 +5,9 @@ library(tidyr)
 library(rvest)
 library(xml2)
 library(stringr)
-library(pbmcapply)
+library(purrr)
+library(furrr)
+library(progressr)
 
 url <- "https://www.zeit.de/gsitemaps/index.xml"
 
@@ -17,114 +19,129 @@ sitemaps <- url |>
 
 sitemaps <- sitemaps[grepl("2020|2021|2022", sitemaps)]
 
-cores <- detectCores()
-article_urls <- pbmclapply(
-  sitemaps, \(x) {
-    x |> 
-      httr::GET() |> 
-      read_html() |> 
-      html_elements("url") |> 
-      html_element("loc") |> 
-      html_text2()
-  },
-  mc.cores = cores, 
-  mc.style = "ETA"
+# Get article urls:
+get_article_urls <- function(x, p) {
+  p()
+  x |> 
+    httr::GET() |> 
+    read_html() |> 
+    html_elements("url") |> 
+    html_element("loc") |> 
+    html_text2()
+}
+
+plan(multisession, workers = parallel::detectCores())
+
+handlers(
+  handler_progress(
+    format = "[:bar] Remaining: :eta"
+  )
 )
+
+with_progress({
+  p <- progressor(steps = length(sitemaps))
+  article_urls <- sitemaps |> future_map(~ get_article_urls(.x, p = p))
+})
+
 article_urls <- do.call(c, article_urls)
 
 # Functions for retrieval from html:
+check2 <- function(x) ifelse(rlang::is_empty(x), NA_character_, x)
+
 get_meta <- function(src, query) {
-  out <- src |> 
+  src |> 
     html_elements(xpath = paste0('//*[@name="', query, '"]')) |>
-    xml_attr("content")
-  if (rlang::is_empty(out)) return(NA_character_)
-  return(out)
+    xml_attr("content") |> 
+    check2()
 }
 
 get_title <- function(src) {
-  out <- src |> 
+  src |> 
     html_element("title") |> 
-    html_text2()
-  if (rlang::is_empty(out)) return(NA_character_)
-  return(out)
+    html_text2() |> 
+    check2()
 }
 
 get_author <- function(src) {
-  out <- src |> 
+  src |> 
     html_elements(xpath = '//a[@rel="author"]') |> 
-    html_text2()
-  if (rlang::is_empty(out)) return(NA_character_)
-  return(out)
+    html_text2() |> 
+    check2()
 }
 
-check_paywall <- function(src) {
-  if (grepl('"paywall": "open",', src))
-    FALSE
-  else
-    TRUE
-}
+# check_paywall <- function(src) {
+#   if (grepl('"paywall": "paid"|"paywall": "register"', src)) 
+#     TRUE
+#   else
+#     FALSE
+# }
+
+check_paywall <- function(src) 
+  ifelse(grepl('"paywall": "paid"|"paywall": "register"', src), TRUE, FALSE)
 
 get_body <- function(src) {
   out <- src |> 
     html_elements(xpath = '//p[@class="paragraph article__item"]') |> 
-    html_text2()
+    html_text2() |> 
+    check2()
   
-  if (length(out) > 1)
-    out <- paste(out, collapse = " ")
+  if (length(out) > 1) out <- paste(out, collapse = " ")
     
-  if (rlang::is_empty(out)) return(NA_character_)
-  return(out)
+  out
 }
 
-# AUTHOR?
+get_author <- function(src) {
+  src |> 
+    html_elements(xpath = '//a[@rel="author"]') |> 
+    html_text2() |> 
+    check2()
+}
 
-zeit_full <- pbmclapply(
-  article_urls[1:100],
-  function(url) {
-    tryCatch(
-      expr = {
-        article <- read_html(httr::GET(url))
-        
-        data.frame(
-          url         = url,
-          title       = get_title(article),
-          description = get_meta(article, "description"),
-          keywords    = get_meta(article, "keywords"),
-          author      = get_author(article),
-          paywall     = check_paywall(article),
-          body        = NA_character_,
-          error       = NA_character_
-        ) |> 
-          mutate(body = ifelse(paywall, NA_character_, get_body(article)))
-      },
-      error = function(e) {
-        e <- as.character(e[1])
-        if (length(e) > 1)
-          e <- paste(e, collapse = " ")
-        
-        data.frame(
-          url         = url,
-          date        = NA_character_,
-          title       = NA_character_,
-          author      = NA_character_,
-          description = NA_character_,
-          keywords    = NA_character_,
-          paywall     = NA_character_,
-          body        = NA_character_,
-          error       = e
-        )
-      }
-    )
-  },
-  mc.cores = cores,
-  mc.style = "ETA"
-)
+# Full scrape: 
+# "news" articles are mostly only agency copy-pastes...?
+#length(article_urls[!grepl("news", article_urls)])
+
+full_scrape <- function(url, p) {
+  p()
+  tryCatch(
+    expr = {
+      article <- read_html(httr::GET(url))
+      
+      data.frame(
+        url         = url,
+        title       = get_title(article),
+        description = get_meta(article, "description"),
+        keywords    = get_meta(article, "keywords"),
+        author      = get_author(article),
+        paywall     = check_paywall(article),
+        body        = NA_character_,
+        error       = NA_character_
+      ) |> 
+        mutate(body = ifelse(paywall, NA_character_, get_body(article)))
+    },
+    error = function(e) {
+      e <- as.character(e[1])
+      if (length(e) > 1)
+        e <- paste(e, collapse = " ")
+      
+      data.frame(
+        url         = url,
+        date        = NA_character_,
+        title       = NA_character_,
+        author      = NA_character_,
+        description = NA_character_,
+        keywords    = NA_character_,
+        paywall     = NA_character_,
+        body        = NA_character_,
+        error       = e
+      )
+    }
+  )
+}
+
+with_progress({
+  p <- progressor(steps = length(article_urls))
+  zeit_full <- article_urls |> future_map(~ full_scrape(.x, p = p))
+})
+
 zeit_full <- tibble(do.call(rbind, zeit_full))
-
-# TESTING
-
-test <- read_html(httr::GET("https://www.zeit.de/politik/ausland/2023-01/schottland-selbstbestimmungsgesetz-transgender-london"))
-test |> 
-  html_elements(xpath = '//a[@rel="author"]') |> 
-  html_text2()
-length(article_urls[!grepl("news", article_urls)])
